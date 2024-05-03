e82346bd517f216de4fcf269d81fa76d9e475795
Mostafizur Rahman <mostafizzfuad@gmail.com>
2024-05-03 22:31:51 +0600

    form validation done...

--- contact_post.php
+++ contact_post.php
@@ -1,6 +1,22 @@
 <?php
 // print_r($_POST);
 
-echo "Name : " . $_POST['visitor_name'] . "<br>";
-echo "Email : " . $_POST['visitor_email'] . "<br>";
-echo "Message : " . $_POST['visitor_message'] . "<br>";
+// echo "Name : " . $_POST['visitor_name'] . "<br>";
+// echo "Email : " . $_POST['visitor_email'] . "<br>";
+// echo "Message : " . $_POST['visitor_message'] . "<br>";
+
+if (empty($_POST['visitor_name'])) {
+    echo "Where is your name ?";
+}
+elseif (empty($_POST['visitor_email'])) {
+    echo "Where is your email ?";
+}
+elseif (!filter_var($_POST['visitor_email'], FILTER_VALIDATE_EMAIL)) {
+    echo "Please provide a valid email address !!";
+} 
+elseif (empty($_POST['visitor_message'])) {
+    echo "Where is your message ?";
+}
+else {
+    echo "Thanks for your submission !!";
+}
